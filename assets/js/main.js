8fb961ee649d09dce033869872f419dc16a8f9b4
Samuele Battistella <battistar92@gmail.com>
2023-08-31 12:25:13 +0200

    Update css selector and html ids

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,28 @@
-const buttonGroup = document.querySelectorAll('.card__rating-button-group button');
-const [ratingCardElement, greetingsCardElement] = document.querySelectorAll('.card');
-const submitButtonElement = document.querySelector('.card__submit-button');
+let currentRate;
+
 const finalRateElement = document.querySelector('#final-rate');
+const ratingButtonElements = document.querySelectorAll(
+  '#rating-button-1, #rating-button-2, #rating-button-3, #rating-button-4, #rating-button-5'
+);
+
+console.log(ratingButtonElements);
+
+for (let i = 0; i < ratingButtonElements.length; i++) {
+  ratingButtonElements[i].addEventListener('click', function () {
+    if (currentRate) {
+      ratingButtonElements[currentRate - 1].classList.remove('selected');
+    }
+    ratingButtonElements[i].classList.add('selected');
+    submitButtonElement.classList.remove('disabled');
+
+    currentRate = i + 1;
+    finalRateElement.textContent = ` ${currentRate} `;
+  });
+}
+
+const ratingCardElement = document.querySelector('#rating-card');
+const greetingsCardElement = document.querySelector('#greetings-card');
+const submitButtonElement = document.querySelector('#submit-button');
 
 submitButtonElement.addEventListener('click', function (e) {
   e.preventDefault();
@@ -9,18 +30,3 @@ submitButtonElement.addEventListener('click', function (e) {
   ratingCardElement.classList.add('hide');
   greetingsCardElement.classList.remove('hide');
 });
-
-let currentRate;
-
-for (let i = 0; i < buttonGroup.length; i++) {
-  buttonGroup[i].addEventListener('click', function () {
-    if (currentRate) {
-      buttonGroup[currentRate - 1].classList.remove('selected');
-    }
-    buttonGroup[i].classList.add('selected');
-    submitButtonElement.classList.remove('disabled');
-
-    currentRate = i + 1;
-    finalRateElement.textContent = ` ${currentRate} `;
-  });
-}
